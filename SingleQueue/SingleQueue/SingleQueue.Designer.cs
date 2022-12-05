3cb3f8ef1e6c7e2857b51e8caf9a4112d9106c6c
Omar Elsaadany <115019564+BugMaker69@users.noreply.github.com>
2022-12-05 17:38:30 +0200

    Add files via upload

---
+++ SingleQueue/SingleQueue/SingleQueue.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace SingleQueue
+{
+    partial class SingleQueue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mean Arrival";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mean Service";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Avg No Of Customer in Service:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Avg No OF Customer Wait In Line: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 244);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Avg Time Spent Wait System:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Avg Wait Time Spend in Line:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Propability ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Utilization System";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "No Customer in Service";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "No Customer Wait Line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(304, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 40);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Time Spent Waiting System";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(304, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 40);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Waiting Time Spent in Line";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(304, 322);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 47);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Propability Customer Given Line";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(203, 398);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Calc All";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(219, 343);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(200, 290);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(200, 244);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 19;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(200, 196);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(200, 147);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 17;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(200, 102);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 13);
+            this.label14.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Average Utilization Of System: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 336);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // SingleQueue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 464);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SingleQueue";
+            this.Text = "SingleQueue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ SingleQueue/SingleQueue/SingleQueue.cs
@@ -0,0 +1,169 @@
+﻿// Copyright © 2022 Omar ElSaadany | All rights reserved
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingleQueue
+{
+    public partial class SingleQueue : Form
+    {
+        public SingleQueue()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void ClearTxt()
+        {
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            label14.Text = "";
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            ClearTxt();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ClearTxt();
+        }
+
+        void ErrorM()
+        {
+            MessageBox.Show("Enter No.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            label14.Text = "";
+            double t4, t3,p;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox2.Text, out t3)))
+            {
+                p=Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                label14.Text = p.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label13.Text = "";
+            double t4, t3, L;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox2.Text, out t3)))
+            {
+                L = Convert.ToDouble(textBox1.Text) / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                label13.Text = L.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label12.Text = "";
+            double t4, t3, L,p,Lq;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox2.Text, out t3)))
+            {
+                p = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                L = Convert.ToDouble(textBox1.Text) / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                Lq = p * L;
+                label12.Text = Lq.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label11.Text = "";
+            double t4, t3,W;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox2.Text, out t3)))
+            {
+                W= 1 / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                label11.Text = W.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label10.Text = "";
+            double t4, t3,p,W,Wq;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox2.Text, out t3)))
+            {
+                p = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                W = 1 / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                Wq = p * W;
+                label10.Text = Wq.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label9.Text="";
+            double t4, t3, p,d=0,t;
+            int n;
+            if (double.TryParse(textBox1.Text, out t4) && (double.TryParse(textBox3.Text, out t3)))
+            {
+                p = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                n = Convert.ToInt32(textBox3.Text);
+
+                for (int i = 0; i <= n; i++)
+                {
+                    d += Math.Pow(p, i);
+
+                }
+                t = (1 - (1 - p) * d);
+                label9.Text = t.ToString();
+            }
+            else { ErrorM(); }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double t1, t2;
+            int t3;
+            if( double.TryParse(textBox1.Text,out t1) && double.TryParse(textBox2.Text, out t2) && int.TryParse(textBox3.Text, out t3))
+            {
+
+                double p = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                double L = Convert.ToDouble(textBox1.Text) / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                double Lq = p * L;
+                double W = 1 / (Convert.ToDouble(textBox2.Text) - Convert.ToDouble(textBox1.Text));
+                double Wq = p * W;
+                int n = Convert.ToInt32(textBox3.Text);
+                double d = 0;
+                double t;
+                for (int i = 0; i <= n; i++)
+                {
+                    d += Math.Pow(p, i);
+
+                }
+                t = (1 - (1 - p) * d);
+
+                MessageBox.Show($"Average Utilization Of System: {p} \n" +
+                    $"Avg No Of Customer in Service: {L} \n" +
+                    $"Avg No OF Customer Wait In Line: {Lq} \n" +
+                    $"Avg Time Spent Wait System: {W} \n" +
+                    $"Avg Wait Time Spend in Line: {Wq} \n" +
+                    $"Propability: {t}");
+            }
+            else { ErrorM(); }
+        }
+    }
+}
+// Copyright © 2022 Omar ElSaadany | All rights reserved
